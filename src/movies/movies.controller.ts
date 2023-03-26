a17bf1b75d996a4bd2f4b300123a32b8e48495e8
kimgenie0409 <kimgenie0409@gmail.com>
2023-03-26 14:14:03 +0900

    [Update] Movies Controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a new movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete the movie id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch the movie id: ${movieId}`;
+  }
+}
